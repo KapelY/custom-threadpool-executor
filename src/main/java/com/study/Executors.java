6f46257155fc3a35bda1a89f1032f5b28402ff3b
Kapelushny <zig.tos@gmail.com>
2021-11-16 11:13:25 +0200

    small fix

--- src/main/java/com/study/Executors.java
+++ src/main/java/com/study/Executors.java
@@ -3,13 +3,11 @@ package com.study;
 import java.util.concurrent.ExecutorService;
 
 public class Executors {
-    int capacity;
-
     public static ExecutorService newFixedThreadPool() {
         return new ThreadPoolExecutor();
     }
     public static ExecutorService newFixedThreadPool(int capacity) {
-        return new ThreadPoolExecutor();
+        return new ThreadPoolExecutor(capacity);
     }
     public static ExecutorService newFixedThreadPool(int capacity, int queue) {
         return new ThreadPoolExecutor(capacity, queue);
